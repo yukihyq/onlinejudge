8f03c2e928f25fabc4e40cad15920741d7f6f56e
Yuki Huang <121444965+yukihyq@users.noreply.github.com>
2023-02-09 22:46:08 +0800

    Create 02.cpp

---
+++ noi.openjudge.cn/ch0101/02.cpp
@@ -0,0 +1,11 @@
+// http://noi.openjudge.cn/ch0101/solution/38576940/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << b << endl;
+    return 0;
+}
