95e351c98e74b06997e7aaf11744950233fd5f48
Yuki Huang <121444965+yukihyq@users.noreply.github.com>
2023-02-19 15:00:20 +0800

    Create 09.cpp

---
+++ noi.openjudge.cn/ch0101/09.cpp
@@ -0,0 +1,25 @@
+//http://noi.openjudge.cn/ch0101/solution/38645119/
+
+#include<iostream>
+using namespace std;
+int main() {
+    char a;
+    a = getchar();
+    cout << "  " << a;
+    cout << endl;
+    cout << " ";
+    for (int i=0;i<3;i++) {
+        cout << a;
+    }
+    cout << endl;
+    for (int i=0;i<5;i++) {
+        cout << a;
+    }
+    cout << endl;
+    cout << " ";
+    for (int i=0;i<3;i++) {
+        cout << a;
+    }
+    cout << endl;
+    cout << "  "<< a << endl;
+}
