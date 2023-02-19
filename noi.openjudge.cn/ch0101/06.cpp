3f67f13c6e55fbe46f644c6b398d205e91ab8c60
Yuki Huang <121444965+yukihyq@users.noreply.github.com>
2023-02-19 14:29:14 +0800

    Create 06.cpp

---
+++ noi.openjudge.cn/ch0101/06.cpp
@@ -0,0 +1,13 @@
+//http://noi.openjudge.cn/ch0101/solution/38644577/
+
+#include<iostream>
+using namespace std;
+int main() {
+    char a;
+    int b;
+    float c;
+    double d;
+    a = getchar();
+    cin >> b >> c >> d;
+    printf("%c %d %0.6f %0.6lf\n",a,b,c,d);
+}
