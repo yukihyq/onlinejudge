3fbb064f9ba1e91203b199bc518a56e23eef4fe0
Yuki Huang <121444965+yukihyq@users.noreply.github.com>
2023-02-19 15:15:54 +0800

    Create 01.cpp

---
+++ noi.openjudge.cn/ch0102/01.cpp
@@ -0,0 +1,9 @@
+//http://noi.openjudge.cn/ch0102/solution/38645454/
+
+#include<iostream>
+using namespace std;
+int main() {
+    int a;
+    short b;
+    cout << sizeof(a) << " " << sizeof(b) << endl;
+}
