3bf3dc0435e389a7a8f66cb3e67d8a2be67b2a3f
Yuki Huang <121444965+yukihyq@users.noreply.github.com>
2023-02-19 14:39:12 +0800

    Create 07.cpp

---
+++ noi.openjudge.cn/ch0101/07.cpp
@@ -0,0 +1,12 @@
+//http://noi.openjudge.cn/ch0101/solution/38644727/
+
+#include<iostream>
+using namespace std;
+int main() {
+    double a;
+    cin >> a;
+    printf("%f\n",a);
+    printf("%0.5f\n",a);
+    printf("%e\n",a);
+    printf("%g\n",a);
+}
