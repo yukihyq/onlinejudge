7c4b463ac76dcf5d35f0bb7a447d174026d31fc6
Yuki Huang <121444965+yukihyq@users.noreply.github.com>
2023-02-26 13:51:41 +0800

    Create 04.cpp

---
+++ noi.openjudge.cn/ch0102/04.cpp
@@ -0,0 +1,11 @@
+// http://noi.openjudge.cn/ch0102/solution/38718761/
+
+#include<iostream>
+using namespace std;
+int main() {
+    // int a = 32768;
+    // short b = a;
+    // printf("%d %d\n", a, b);
+
+    cout << "D C" << endl;
+}
