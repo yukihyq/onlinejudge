c0508f04adb512e0adcee69dfad54d8078b51900
Yuki Huang <121444965+yukihyq@users.noreply.github.com>
2023-02-26 15:20:02 +0800

    Create 06.cpp

---
+++ noi.openjudge.cn/ch0103/06.cpp
@@ -0,0 +1,9 @@
+// http://noi.openjudge.cn/ch0103/solution/38720558/
+
+#include<iostream>
+using namespace std;
+int main() {
+    double a,b;
+    cin >> a >> b;
+    printf("%0.3lf%\n",b*100/a);
+}
