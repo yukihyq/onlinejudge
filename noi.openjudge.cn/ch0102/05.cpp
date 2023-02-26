5ec8967a37d5a0941389e77e47935fd9b0df814b
Yuki Huang <121444965+yukihyq@users.noreply.github.com>
2023-02-26 13:48:47 +0800

    Create 05.cpp

---
+++ noi.openjudge.cn/ch0102/05.cpp
@@ -0,0 +1,10 @@
+//http://noi.openjudge.cn/ch0102/solution/38718698/
+
+#include<iostream>
+using namespace std;
+int main() {
+    //double a = 1.000000001;
+    //float b = a;
+    //printf("%.9f %.9f\n", a, b);
+    cout << "F E" << endl;
+}
