ce7df46d7a1d015c761e97c2fce9c7ee4a865e60
Yuki Huang <121444965+yukihyq@users.noreply.github.com>
2023-02-26 15:04:20 +0800

    Create 02.cpp

---
+++ noi.openjudge.cn/ch0103/02.cpp
@@ -0,0 +1,9 @@
+// http://noi.openjudge.cn/ch0103/solution/38720211/
+
+#include<iostream>
+using namespace std;
+int main() {
+    int a,b,c;
+    cin >> a >> b >> c;
+    cout << (a + b) * c << endl;
+}
