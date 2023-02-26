0994abc02eac6db1a39860d700a23edb8227cba5
Yuki Huang <121444965+yukihyq@users.noreply.github.com>
2023-02-26 14:14:27 +0800

    Create 09.cpp

---
+++ noi.openjudge.cn/ch0102/09.cpp
@@ -0,0 +1,14 @@
+// http://noi.openjudge.cn/ch0102/solution/38719225/
+
+#include<iostream>
+using namespace std;
+int main() {
+    int a,b;
+    cin >> a;
+    if (a == 0) {
+        b = 0;
+    } else {
+        b = 1;
+    }
+    cout << b;
+}
