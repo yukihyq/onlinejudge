c9c2af150573cb6e84e190bba8d585a547756c38
Yuki Huang <121444965+yukihyq@users.noreply.github.com>
2023-02-26 15:06:35 +0800

    Create 04.cpp

---
+++ noi.openjudge.cn/ch0103/04.cpp
@@ -0,0 +1,9 @@
+// http://noi.openjudge.cn/ch0103/solution/38720304/
+
+#include<iostream>
+using namespace std;
+int main() {
+    int a,b;
+    cin >> a >> b;
+    cout <<  a / b << " " << a % b << endl;
+}
