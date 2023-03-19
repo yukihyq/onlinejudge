5e5e11d21b9cbe887f3756f6652cf510e2b3db34
Yuki Huang <121444965+yukihyq@users.noreply.github.com>
2023-03-19 21:14:20 +0800

    Create 02.cpp

---
+++ noi.openjudge.cn/ch0104/02.cpp
@@ -0,0 +1,13 @@
+// http://noi.openjudge.cn/ch0104/solution/39077926/
+
+#include<iostream>
+using namespace std;
+int main() {
+    long long n;
+    cin >> n;
+    if (n % 2 == 1) {
+        cout << "odd" << endl;
+    } else {
+        cout << "even" << endl;
+    }
+}
