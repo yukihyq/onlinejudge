3e193eb3786c192c3c65eaf38ac7c49afce6747c
Yuki Huang <121444965+yukihyq@users.noreply.github.com>
2023-03-21 22:42:00 +0800

    Create 06.cpp

---
+++ noi.openjudge.cn/ch0104/06.cpp
@@ -0,0 +1,13 @@
+// http://noi.openjudge.cn/ch0104/solution/39105518/
+
+#include<iostream>
+using namespace std;
+int main() {
+    long long a;
+    cin >> a;
+    if (a >= 10 && a <= 99) {
+        cout << "1" << endl;
+    } else {
+        cout << "0" << endl;
+    }
+}
