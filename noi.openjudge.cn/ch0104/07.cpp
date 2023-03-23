9f6196d22eea116194f5b77d4bb8340da830711b
Yuki Huang <121444965+yukihyq@users.noreply.github.com>
2023-03-23 23:04:20 +0800

    Create 07.cpp

---
+++ noi.openjudge.cn/ch0104/07.cpp
@@ -0,0 +1,13 @@
+// http://noi.openjudge.cn/ch0104/solution/39112487/
+
+#include<iostream>
+using namespace std;
+int main() {
+    int a,b;
+    cin >> a >> b;
+    if (a >= 10 || b >= 20) {
+        cout << "1";
+    } else {
+        cout << "0";
+    }
+}
