c3e310471452cd3054e3358291a571bb8b633544
Yuki Huang <121444965+yukihyq@users.noreply.github.com>
2023-03-23 23:05:18 +0800

    Create 08.cpp

---
+++ noi.openjudge.cn/ch0104/08.cpp
@@ -0,0 +1,13 @@
+// http://noi.openjudge.cn/ch0104/solution/39112526/
+
+#include<iostream>
+using namespace std;
+int main() {
+    int a;
+    cin >> a;
+    if (a % 3 == 0 && a % 5 == 0) {
+        cout << "YES";
+    } else {
+        cout << "NO";
+    }
+}
