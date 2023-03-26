48a4a4d301c864dad1c63f76dd0e0b693e15dc8f
Yuki Huang <121444965+yukihyq@users.noreply.github.com>
2023-03-26 14:31:20 +0800

    Create 15.cpp

---
+++ noi.openjudge.cn/ch0103/15.cpp
@@ -0,0 +1,10 @@
+// http://noi.openjudge.cn/ch0103/solution/38940974/
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main() {
+    float n,x,y;
+    cin >> n >> x >> y;
+    cout << int(n-y/x) << endl;
+}
