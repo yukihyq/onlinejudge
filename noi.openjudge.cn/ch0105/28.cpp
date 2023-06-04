5212b9609661f30350e7e34e5a7ba7c8ede6723e
Yuki Huang <121444965+yukihyq@users.noreply.github.com>
2023-06-04 23:17:50 +0800

    Update 28.cpp

--- noi.openjudge.cn/ch0105/28.cpp
+++ noi.openjudge.cn/ch0105/28.cpp
@@ -1,1 +1,13 @@
+// http://noi.openjudge.cn/ch0105/solution/40124897/
 
+#include<iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    while (n >= 10 && n != 0) {
+        cout << n % 10 << " ";
+        n = n / 10;
+    }
+    cout << n % 10 << endl;
+}
